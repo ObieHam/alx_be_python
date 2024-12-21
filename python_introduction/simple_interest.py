bee6185372afa684509bfc181ad32c0faed214b9
ObieHam <165133135+ObieHam@users.noreply.github.com>
2024-12-21 20:44:15 +0300

    Add files via upload

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+# The iniital investment
+principal = 1000
+# The interest rate in decimal
+rate = 0.05
+# The number of years the interest had to accumulate
+time = 3
+print ("The simple interest is", str (principal * rate * time) + ".",) 
