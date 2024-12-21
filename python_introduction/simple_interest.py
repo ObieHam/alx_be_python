4c22f4eb43c8952ad26d239db4f4ada7fede6439
ObieHam <165133135+ObieHam@users.noreply.github.com>
2024-12-21 20:47:02 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -4,4 +4,4 @@ principal = 1000
 rate = 0.05
 # The number of years the interest had to accumulate
 time = 3
-print ("The simple interest is", str (principal * rate * time))
+print ("The simple interest is: ", str (principal * rate * time)) 
