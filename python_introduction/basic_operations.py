dc64631fb77bbad4547521de039b15dd8de05dd3
ObieHam <165133135+ObieHam@users.noreply.github.com>
2024-12-21 20:34:59 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,14 @@
-
+# The value of the first variable
+number1 = 10
+# The value of the second variable
+number2 = 5
+# The result of the addition of the two variables
+AdditionResult = number1 + number2
+# The result of the subtraction of the two variables 
+SubtractionResult = number1 - number2
+# The result of the Multiplication of the two variables
+MultiplicationResult = number1 * number2
+# Printing the result of each of the operations
+print("Addition","of", number1, "and", number2, "is", str (AdditionResult) + "." )
+print("Subtraction","of", number1, "and", number2, "is", str (SubtractionResult) + "." )
+print("Multiplication","of", number1, "and", number2, "is", str (MultiplicationResult) + "." )
