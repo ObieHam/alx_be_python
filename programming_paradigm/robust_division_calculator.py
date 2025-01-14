dd03058abe091310144eb79f8c17157cc5090a0d
ObieHam <165133135+ObieHam@users.noreply.github.com>
2025-01-14 14:32:48 +0300

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,4 +6,5 @@ def safe_divide(numerator, denominator):
     except ValueError:
         return "Error: Please enter numeric values only."
     else:
-        print(numerator/denominator)
+        result = float(numerator)/float(denominator)
+        print(f"The result of the division is {float}")
