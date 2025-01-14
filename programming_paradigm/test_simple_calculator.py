f8657e7d81edc7e1cb9c48ba61dd8c53aefcddfe
ObieHam <165133135+ObieHam@users.noreply.github.com>
2025-01-14 22:13:11 +0300

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,36 +4,32 @@ from simple_calculator import SimpleCalculator
 class SimpleCalculatorTest(unittest.TestCase):
     def setUp(self):
         """Create a SimpleCalculator instance."""
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()  # Note the attribute is renamed to `self.calc`
 
-    def test_add(self):
-        self.assertEqual(self.calculator.add(1, 1), 2)
-        self.assertEqual(self.calculator.add(1, -1), 0)
-        self.assertEqual(self.calculator.add(-1, -1), -2)
-        self.assertEqual(self.calculator.add(0.5, 0.5), 1)
+    def test_addition(self):
+        """Test addition operation."""
+        self.assertEqual(self.calc.add(1, 1), 2)
+        self.assertEqual(self.calc.add(1, -1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
+        self.assertEqual(self.calc.add(0.5, 0.5), 1)
 
-    def test_subtract(self):
-        self.assertEqual(self.calculator.subtract(1, 1), 0)
-        self.assertEqual(self.calculator.subtract(-1, 1), -2)
-        self.assertEqual(self.calculator.subtract(-1, -1), 0)
-        self.assertEqual(self.calculator.subtract(1, -1), 2)
-        self.assertEqual(self.calculator.subtract(1, 0.5), 0.5)
+    def test_subtraction(self):
+        """Test subtraction operation."""
+        self.assertEqual(self.calc.subtract(1, 1), 0)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(1, -1), 2)
+        self.assertEqual(self.calc.subtract(1, 0.5), 0.5)
 
-    def test_multiply(self):
-        self.assertEqual(self.calculator.multiply(1, 1), 1)
-        self.assertEqual(self.calculator.multiply(-1, 1), -1)
-        self.assertEqual(self.calculator.multiply(-1, -1), 1)
-        self.assertEqual(self.calculator.multiply(1, -1), -1)
-        self.assertEqual(self.calculator.multiply(1, 0.5), 0.5)
-        self.assertEqual(self.calculator.multiply(0, 0.5), 0)
+    def test_multiplication(self):
+        """Test multiplication operation."""
+        self.assertEqual(self.calc.multiply(1, 1), 1)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
+        self.assertEqual(self.calc.multiply(1, -1), -1)
+        self.assertEqual(self.calc.multiply(1, 0.5), 0.5)
+        self.assertEqual(self.calc.multiply(0, 0.5), 0)
 
-    def test_divide(self):
-        self.assertEqual(self.calculator.divide(1, 1), 1)
-        self.assertEqual(self.calculator.divide(1, -1), -1)
-        self.assertEqual(self.calculator.divide(1, 0), None)
-        self.assertEqual(self.calculator.divide(0, 1), 0)
-        self.assertEqual(self.calculator.divide(1, 0.5), 2)
-        self.assertEqual(self.calculator.divide(5, 2), 2.5)
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_division(self):
+        """Test division operation."""
+       
